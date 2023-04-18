d73c7d06778a10d9a4f7274a1d6750ee44601b2c
rafael my master <sorafaelbook@gmail.com>
2023-04-18 02:05:52 -0500

    asd

--- trsureSec.js
+++ trsureSec.js
@@ -2,22 +2,22 @@ const trsureSec = [
     {
         isOpening: false,itemType:"treasure",magRes: 10, meshId: 'bonus1', 
         name: "chiefplate", openingBy: undefined, place: "swampforest", plusDef: 15,
-        plusDmg: 0, price: 20000, qnty: 1, x: 4, z: -32
+        durability: 100, cState: 100, plusDmg: 0, price: 20000, qnty: 1, x: 4, z: -32
     },
     {
         isOpening: false,itemType:"treasure",magRes: 10, meshId: 'bonus2', 
         name: "karba", openingBy: undefined, place: "swampforest", plusDef: 40,
-        plusDmg: 0, price: 20000, qnty: 1, x: -40, z: 70
+        durability: 200, cState: 200, plusDmg: 0, price: 20000, qnty: 1, x: -40, z: 70
     },
     {
         isOpening: false,itemType:"treasure",magRes: 10, meshId: 'bonus3', 
         name: "hydra", openingBy: undefined, place: "swampforest", plusDef: 35,
-        plusDmg: 0, price: 20000, qnty: 1, x: 50, z: -70
+        durability: 180, cState: 180, plusDmg: 0, price: 20000, qnty: 1, x: 50, z: -70
     },
     {
         isOpening: false,itemType:"treasure",magRes: 10, meshId: 'bonus4', 
         name: "grimblue", openingBy: undefined, place: "swampforest", plusDef: 0,
-        plusDmg: 40, price: 20000, qnty: 1, x: 60, z: -68
+        durability: 300, cState: 300, plusDmg: 40, price: 20000, qnty: 1, x: 60, z: -68
     },
 ]
 
